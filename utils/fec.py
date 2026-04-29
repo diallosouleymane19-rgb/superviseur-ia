a7621f78260b6abb442dbb307e0c8b280a44c7f4
diallosouleymane19-rgb <diallosouleymane19@gmail.com>
2026-04-29 06:13:23 +0200

    Update fec.py

--- utils/fec.py
+++ utils/fec.py
@@ -1,1 +1,11 @@
+import datetime
+
+def generer_fec():
+    filename = f"FEC_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write("Journal\tDate\tCompte\tLibellé\tDébit\tCrédit\n")
+        f.write("AC\t20240101\t606\tAchat fournitures\t100.00\t0.00\n")
+
+    return filename
 
