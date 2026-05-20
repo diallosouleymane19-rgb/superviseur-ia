16412b2d0f5845d973654b747da831039f92f2f9
diallosouleymane19-rgb <diallosouleymane19@gmail.com>
2026-05-20 22:00:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ from utils.plan_financement import (
     calculer_kpi_financiers, 
     generer_graphique_waterfall, 
     export_excel_complet,
-    generer_conseils_experts
+    generer_conseils_experts,
 )
 from utils.tft import page_tft
 from utils.comparatif import page_comparatif
